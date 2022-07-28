5ed3d7f19662d203a4441690bd9976985759cb2c
TracK92 <40690789+TracK92@users.noreply.github.com>
2022-07-28 23:40:00 +0300

    Update my test to use fetchmock to count items on home page

--- src/__test__/hompageCounter.test.js
+++ src/__test__/hompageCounter.test.js
@@ -1,24 +1,22 @@
- /**
+/**
  * @jest-environment jsdom
  */
 
-  import countitems from '../modules/homepageCounter.js';
-  import fetchMock from 'jest-fetch-mock';
-  fetchMock.enableMocks();
+const fetchMock = require('fetch-mock');
 
-  
-  test('returns the count', async () => {   
-                const myArray = [
-                    { id: 1, title: 'Moana'},
-                    { id: 2, title: 'Mulan'},
-                    { id: 3, title: 'The Lion King'},
-                    { id: 4, title: 'Eternal Sunshine of the Spotless Mind'},
-                  ];
-                
-    fetch.mockResponseOnce(myArray)
-
-    const counts = await countitems();
-    expect(counts).toHaveLength(18);
-    
+describe('count all items on page (they have < 20 characters)', () => {
+  test('can count items on page', async () => {
+    fetchMock.get('https://yts.mx/api/v2/list_movies.json?genre=animation&limit=25&sort_by=download_count&minimum_rating=7', {
+      anime: [
+        { title: 'Moana' },
+        { title: 'Mulan' },
+        { title: 'The Lion King' },
+        { title: 'Eternal Sunshine of the Spotless Mind' },
+        { title: 'Pirates of the Caribbean: The Curse of the Black Pearl' },
+      ].filter((item) => item.title.length < 20),
+    });
+    const response = await fetch('https://yts.mx/api/v2/list_movies.json?genre=animation&limit=25&sort_by=download_count&minimum_rating=7');
+    const res = await response.json();
+    expect(res.anime).toHaveLength(3);
   });
-
+});
--- src/modules/homepageCounter.js
+++ src/modules/homepageCounter.js
@@ -1,26 +1,12 @@
 const counter = document.querySelector('.counter');
 
-// const countAnimations = (data) => {
-//   data = data.filter((item) => item.title.length < 20);
-//   counter.innerHTML = `Animation Films (${data.length})`;
-// };
-
-// const countitems = async () => {
-//   await fetch('https://yts.mx/api/v2/list_movies.json?genre=animation&limit=25&sort_by=download_count&minimum_rating=7')
-//     .then((response) => response.json())
-//     .then((json) => countAnimations(json.data.movies))
-//     .catch((err) => console.log(err));
-// };
-
-
 const countitems = async () => {
   await fetch('https://yts.mx/api/v2/list_movies.json?genre=animation&limit=25&sort_by=download_count&minimum_rating=7')
-  .then((response) => response.json())
+    .then((response) => response.json())
     .then((json) => {
       const movies = (json.data.movies).filter((item) => item.title.length < 20);
       const count = movies.length;
       counter.innerHTML = `Animation Films (${count})`;
-    })
-    
-}
+    });
+};
 export default countitems;
